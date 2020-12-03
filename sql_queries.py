10af2f634cbe724a89125209965053d8a426f8c3
ralf-r-r <ralf@Ralfs-MacBook-Pro.local>
2020-12-03 19:01:52 +0100

    updates sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -38,24 +38,24 @@ staging_songs(
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS
 songplays (
-    songplay_id INTEGER IDENTITY(1,1), start_time TIMESTAMP NOT NULL ,user_id INTEGER NOT NULL,
+    songplay_id INTEGER IDENTITY(1,1) PRIMARY KEY, start_time TIMESTAMP NOT NULL ,user_id INTEGER NOT NULL,
     level VARCHAR, song_id VARCHAR NOT NULL, artist_id VARCHAR NOT NULL, 
-    session_id INTEGER NOT NULL, location VARCHAR, user_agent VARCHAR
+    session_id INTEGER, location VARCHAR, user_agent VARCHAR
 )
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS
 users(
-    user_id INTEGER NOT NULL, first_name VARCHAR NOT NULL, last_name VARCHAR NOT NULL, 
-    gender VARCHAR, level VARCHAR NOT NULL
+    user_id INTEGER PRIMARY KEY NOT NULL, first_name VARCHAR, last_name VARCHAR, 
+    gender VARCHAR, level VARCHAR
 )
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS
 songs(
-    song_id VARCHAR NOT NULL, title VARCHAR NOT NULL, artist_id VARCHAR NOT NULL, 
+    song_id VARCHAR PRIMARY KEY NOT NULL, title VARCHAR, artist_id VARCHAR NOT NULL, 
     year INTEGER, duration DOUBLE PRECISION
 )
 """)
@@ -63,7 +63,7 @@ songs(
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS
 artists(
-    artist_id VARCHAR NOT NULL, name VARCHAR NOT NULL, location VARCHAR, lattitude DOUBLE PRECISION, 
+    artist_id VARCHAR PRIMARY KEY NOT NULL, name VARCHAR, location VARCHAR, lattitude DOUBLE PRECISION, 
     longitude DOUBLE PRECISION
 )
 """)
@@ -71,8 +71,8 @@ artists(
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS
 time(
-    start_time TIMESTAMP NOT NULL, hour INTEGER NOT NULL, day INTEGER NOT NULL, week INTEGER NOT NULL, 
-    month INTEGER NOT NULL, year INTEGER NOT NULL, weekday INTEGER NOT NULL
+    start_time TIMESTAMP PRIMARY KEY NOT NULL, hour INTEGER, day INTEGER, week INTEGER, 
+    month INTEGER, year INTEGER, weekday INTEGER
 )
 """)
 
@@ -156,4 +156,3 @@ create_table_queries = [staging_events_table_create, staging_songs_table_create,
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
-
